4bcafcece62d51800601f20ced89f055739df6d6
Genie <genie@ye.park-EM-01>
2026-06-16 23:57:49 +0900

    fix: add netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mss-pd-planner/',
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
